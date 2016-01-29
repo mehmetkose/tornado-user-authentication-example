930f03237de8d284f20a9607a5b0a601707fc5f7
mehmet kose <mehmetkose122@gmail.com>
2016-01-29 23:38:31 +0200

    blocker incorrect login attempts

--- app.py
+++ app.py
@@ -11,30 +11,34 @@ from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
 
 class BaseHandler(tornado.web.RequestHandler):
-	def get_current_user(self):
-		return self.get_secure_cookie("user")
-		
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+        
 class MainHandler(BaseHandler):
-	@tornado.web.authenticated
-	def get(self):
-		self.render('index.html', user=self.current_user)
+    @tornado.web.authenticated
+    def get(self):
+        incorrect = self.get_secure_cookie("incorrect") or 0
+        if int(incorrect) > 20:
+            self.write('<center>blocked</center>')
+            return
+        self.render('index.html', user=self.current_user)
 
 class LoginHandler(BaseHandler):
-	def get(self):
-		self.render('login.html')
-	def post(self):
-		getusername = self.get_argument("username")
-		getpassword = self.get_argument("password")
-		# TODO : Check data from DB
-		if "demo" == getusername and "demo" == getpassword:
-		    self.set_secure_cookie("user", self.get_argument("username"))
-		    self.redirect(self.reverse_url("main"))
-		else:
-		    wrong = self.get_secure_cookie("wrong")
-		    if not wrong:
-		    	wrong = 0
-		    self.set_secure_cookie("wrong", str(int(wrong)+1))
-		    self.write('Something Wrong With Your Data <a href="/login">Back</a> '+str(wrong))
+    def get(self):
+        self.render('login.html')
+    def post(self):
+        getusername = self.get_argument("username")
+        getpassword = self.get_argument("password")
+        # TODO : Check data from DB
+        if "demo" == getusername and "demo" == getpassword:
+            self.set_secure_cookie("user", self.get_argument("username"))
+            self.redirect(self.reverse_url("main"))
+        else:
+            incorrect = self.get_secure_cookie("incorrect")
+            if not incorrect:
+                incorrect = 0
+            self.set_secure_cookie("incorrect", str(int(incorrect)+1))
+            self.write('Something Wrong With Your Data <a href="/login">Back</a> '+str(incorrect))
 
 class LogoutHandler(BaseHandler):
     def get(self):
@@ -47,12 +51,12 @@ class Application(tornado.web.Application):
         settings = {
             "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
             "login_url": "/login",
-			'template_path': os.path.join(base_dir, "templates"),
-			'static_path': os.path.join(base_dir, "static"),
-			'debug':True,
-			"xsrf_cookies": True,
-		}
-		
+            'template_path': os.path.join(base_dir, "templates"),
+            'static_path': os.path.join(base_dir, "static"),
+            'debug':True,
+            "xsrf_cookies": True,
+        }
+        
         tornado.web.Application.__init__(self, [
             tornado.web.url(r"/", MainHandler, name="main"),
             tornado.web.url(r'/login', LoginHandler, name="login"),
